2bd26fdad22bf69068f4ce4a0e731493b110615d
alinekabbas <alinekabbas@gmail.com>
2022-11-23 11:11:31 -0300

    exercicio completo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
+import { goToHomePage, goToProfilePage, goToLoginPage } from "../Router/coordinator";
+import { useNavigate } from "react-router-dom";
+
 function Header() {
+  const navigate = useNavigate()
 
   return (
     <header>
-      <button >
+      <button onClick={() => goToHomePage(navigate)} >
         Ir para página inicial
       </button>
-      <button >
+      <button onClick={() => goToProfilePage(navigate, 'Aline')}>
         Ir para página de perfil
+      </button>
+      <button onClick={() => goToLoginPage(navigate)} >
+        Ir para página de login
       </button>
     </header>
   );
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+function LoginPage() {
+  return (
+    <>
+        <Header/>
+        <h1>Login Page</h1>
+    </>
+  );
+}
+
+export default LoginPage;
